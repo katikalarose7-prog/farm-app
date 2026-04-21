fefed2be7824b53c927a17939b333476ced44dea
Rose378 <rosemary.katikala7@gmail.com>
2026-04-21 16:32:06 +0530

    Farm Management App13

--- farm-backend/models/productionCategory.js
+++ farm-backend/models/productionCategory.js
@@ -1,11 +1,13 @@
 // farm-backend/models/ProductionCategory.js
 const mongoose = require('mongoose');
 
+
 const productionCategorySchema = new mongoose.Schema({
   name:      { type: String, required: true, trim: true },
   unit:      { type: String, required: true, trim: true },
   emoji:     { type: String, default: '📦' },
   isDefault: { type: Boolean, default: false }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('ProductionCategory', productionCategorySchema);
